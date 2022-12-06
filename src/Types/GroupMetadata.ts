dc32a22180c15e702415c7f0652bf30b26651210
Leonardo Bein <leonardobein@gmail.com>
2022-12-06 13:58:45 -0300

    feat: updated extractGroupMetadata for community

--- src/Types/GroupMetadata.ts
+++ src/Types/GroupMetadata.ts
@@ -16,6 +16,7 @@ export interface GroupMetadata {
     desc?: string
     descOwner?: string
     descId?: string
+    descTime?: number
     /** is set when the group only allows admins to change group settings */
     restrict?: boolean
     /** is set when the group only allows admins to write messages */
@@ -29,6 +30,10 @@ export interface GroupMetadata {
     /** the person who added you */
     author?: string
     iconImg?: string
+
+    community?: boolean
+    communityId?: string
+    communityDefaultGroup?: boolean
 }
 
 
